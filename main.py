169c6925c8a10f94dfa3f3ef6249b876b64246a2
Dmitry G <103391790+dmitryg65@users.noreply.github.com>
2022-04-11 23:33:42 +0000

    03 - 1

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-my_list[-1:] -> [2187]
-my_list[:] -> [3, 9, 27, 81, 243, 729, 2187]
-my_list[2:-1:-3] -> []
-my_list[2:0:-3] -> [27]
-my_list[7]-> IndexError
+NK = list(map(int, input().split()))
+N, K = NK[0], NK[1]
+print((N + K) * (N - K))
