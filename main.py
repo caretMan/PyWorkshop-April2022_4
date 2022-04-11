40da5e635f196356c0906fcf86778818a4a31b60
Dmitry Grigorev <103391790+dmitryg65@users.noreply.github.com>
2022-04-11 01:04:56 +0000

    01 - 7

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Answer: b, d")
+income = int(input())
+if income <= 15527:
+  percent = 0
+elif income <= 42707:
+  percent = 15
+elif income <= 132406:
+  percent = 25
+else:
+  percent = 28
+calculated_tax = '%.0f' % (income * percent / 100)
+print(f'The tax for {income} is {percent}%. That is {calculated_tax} dollars!')
