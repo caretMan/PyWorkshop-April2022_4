b2f04010a84d76bfb70d205e11d413dea7b0d9fb
Dmitry Grigorev <103391790+dmitryg65@users.noreply.github.com>
2022-04-11 01:06:48 +0000

    01 - 8

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-income = int(input())
-if income <= 15527:
-  percent = 0
-elif income <= 42707:
-  percent = 15
-elif income <= 132406:
-  percent = 25
-else:
-  percent = 28
-calculated_tax = '%.0f' % (income * percent / 100)
-print(f'The tax for {income} is {percent}%. That is {calculated_tax} dollars!')
+print('The output of this code is: \n\nAngelina Jolie and Leonardo DiCaprio met yesterday in Paris.')
