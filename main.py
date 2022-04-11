77497ff9cd03c98e0d4b17b14880034e19397863
Dmitry G <103391790+dmitryg65@users.noreply.github.com>
2022-04-11 23:54:57 +0000

    03 - 4

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-NK = list(map(int, input().split()))
-N, K = NK[0], NK[1]
-print(N - K % N)
+wtT = list(map(int, input().split()))
+w, t, T = wtT[0], wtT[1], wtT[2]
+if T % t == 0:
+  print(T // t * w)
+else:
+  print((T // t + 1) * w)
