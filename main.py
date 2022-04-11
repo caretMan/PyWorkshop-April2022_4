c040bcb6cb14d4950cb40a9699e048b8553970a3
Dmitry G <103391790+dmitryg65@users.noreply.github.com>
2022-04-11 23:58:27 +0000

    03 - 5

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-wtT = list(map(int, input().split()))
-w, t, T = wtT[0], wtT[1], wtT[2]
-if T % t == 0:
-  print(T // t * w)
+N = int(input())
+if N % 2 == 0:
+  print(N // 2)
 else:
-  print((T // t + 1) * w)
+  print(N)
