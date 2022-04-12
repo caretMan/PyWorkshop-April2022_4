ea86e7967c7a7b7ba2872bada1d4ac34a8e792f2
Dmitry G <103391790+dmitryg65@users.noreply.github.com>
2022-04-12 02:30:40 +0000

    04 - 2

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from random import randint
-from string import ascii_lowercase
-letters = [ascii_lowercase[randint(0, 25)] for _ in range(20)]
-vowels = ['a', 'e', 'i', 'o', 'u']
-def choose_vowels(letter):
-  if letter in vowels:
-    return(True)
-print(list(filter(choose_vowels, letters)))
+even = [0, 2, 4, 6, 8]
+odd = [1, 3, 5, 7, 9]
+
+my_sum = list(map(lambda a, b: a + b, even, odd))
+
+remainders = list(map(lambda x: x % 3, my_sum))
+
+nonzero_remainders = list(filter(lambda r: (r + 1) // 2, remainders))
