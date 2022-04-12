9801542f94117af0f8cd750582636d15e38621f9
Dmitry G <103391790+dmitryg65@users.noreply.github.com>
2022-04-12 02:58:51 +0000

    04 - 4

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-from random import randint
-Candidates = ['Денис', 'Дима', 'Ваня', 'Дима', 'Дима', 'Лёха', 'Вова', 'Вадим', 'Богдан', 'Юра', 'Антон', 'Артем', 'Костя']
-Maths = [randint(25, 50) for _ in Candidates]
-Physics = [randint(25, 50) for _ in Candidates]
-English = [randint(25, 50) for _ in Candidates]
-Results = list(map(lambda a, b, c, d: [a, b + c + d], Candidates, Maths, Physics, English))
-def is_admitted(result):
-  if int(result[1]) >= 100:
-    return True
-Admitted = list(filter(is_admitted, Results))
-print(list(map(lambda x: x[0], Admitted)))
+a, c
