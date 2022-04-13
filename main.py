d23150adc651bfedb8df10dff8a81e5f5f97b25a
Dmitry G <103391790+dmitryg65@users.noreply.github.com>
2022-04-13 23:32:20 +0000

    Domino, stage 2

--- main.py
+++ main.py
@@ -1,49 +1,51 @@
-# Tasks 1 - 3:
-# (Работает для списков как с неповторяющимися, так и с повторяющимися числами)
-
 from random import randint
 
 
-def list_move(list: list):
-  move = randint(0, len(list) - 1)
-  return list[-move:] + list[:-move]
+while True:
+  Stock = [[i, j] for i in range(7) for j in range(i, 7)]
+  Player = []
+  Computer = []
+  for i in range(7):
+    CurrentPiece = randint(0, len(Stock) - 1)
+    Player.append(Stock[CurrentPiece])
+    Stock.pop(CurrentPiece)
+  for i in range(7):
+    CurrentPiece = randint(0, len(Stock) - 1)
+    Computer.append(Stock[CurrentPiece])
+    Stock.pop(CurrentPiece)
+  if [6, 6] not in Stock or [5, 5] not in Stock:
+    break
 
+if [6, 6] not in Stock:
+  Snake = [[6, 6]]
+else:
+  Snake = [[5, 5]]
+if Snake[0] in Player:
+  Player.remove(Snake[0])
+  Status = 'computer'
+else:
+  Computer.remove(Snake[0])
+  Status = 'player'
 
-def binary_search_max(list:list):
-    if len(list) < 2:
-        return list[0]
-    else:
-        if list[0] > list[len(list) // 2]:
-            MaxElement = binary_search_max(list[:len(list) // 2])
-            return MaxElement
-        else:
-            MaxElement = binary_search_max(list[len(list) // 2:])
-            return MaxElement
+# print(f'Stock pieces: {Stock}\nComputer pieces: {Computer}\nPlayer pieces: {Player}\nDomino snake: {Snake}\nStatus: {Status}')
 
+print('================================================================================')
 
-def binary_search_min(list:list):
-    if len(list) < 2:
-        return list[0]
-    else:
-        if list[len(list) // 2 - 1] <= list[-1]:
-            MinElement = binary_search_min(list[:len(list) // 2])
-            return MinElement
-        else:
-            MinElement = binary_search_min(list[len(list) // 2:])
-            return MinElement
+print(f'Stock size: {len(Stock)}')
 
+print(f'Computer pieces: {len(Computer)}')
 
-given_list = [randint(0, 1000) for _ in range(13)]
-given_list.sort()
-given_list = list_move(given_list)
-print(given_list)
-MaxElement = binary_search_max(given_list)
-print(f'Max = {MaxElement}')
-MinElement = binary_search_min(given_list)
-print(f'Min = {MinElement}')
+Snake_to_print = ''
+for i, piece in enumerate(Snake, start = 1):
+  Snake_to_print += str(piece)
+  if i != len(Snake):
+    Snake_to_print += ' '
+print(f'\n{Snake_to_print}\n\nYour pieces:')
 
-# Task 4: log(N)
-
-# Task 5: нет
-
-# Task 6: Разбиваем список на 2 равные части (или, в случае списка длиной 2n+1 нечетной длины, разбиваем его на 2 части длины n и n+1). Затем сравниваем первые элементы данных частей, и та часть, у которой первый элемент больше, и содержит в себе максимальный элемент. Если рассматриваемые первые элементы частей равны, то берем тот, который ближе к концу в исходном списке. Это работает, поскольку числа отсортированы (пусть и сдвинуты на рандомное число позиций), то есть относительно какого-либо числа перед тем, как будет максимум, числа будут исключительно возрастать; после максимума не может встретиться число, меньшее рассматриваемого.
+for i, piece in enumerate(Player, start = 1):
+  print(f'{i}:{piece}')
+  
+if Status == 'player':
+  print(f"\nStatus: It's your turn to make a move. Enter your command.")
+else:
+  print(f"\nStatus: Computer is about to make a move. Press Enter to continue...")
