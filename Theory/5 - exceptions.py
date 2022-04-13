e157b6625bc9feed942919be3084e3c513871348
Dmitry G <103391790+dmitryg65@users.noreply.github.com>
2022-04-13 01:10:11 +0000

    05 - 4

---
+++ Theory/5 - exceptions.py
@@ -0,0 +1,86 @@
+print('Hello')
+print(10 / 0)  # code goes till first exception
+
+# typical
+
+# NameError
+a = 5; print(a + b)  # b is not defined before call
+
+# TypeError
+print('15' + 2)  # operation or func is applied to OBJ of wrong type
+
+# ValueError
+print(int('five'))  # OBJ type is ok but value inappropriate
+
+# PYTHON exception tree
+# BaseException
+#  +-- SystemExit
+#  +-- KeyboardInterrupt
+#  +-- GeneratorExit
+#  +-- Exception
+#       +-- StopIteration
+#       +-- StopAsyncIteration
+#       +-- ArithmeticError
+#       |    +-- FloatingPointError
+#       |    +-- OverflowError
+#       |    +-- ZeroDivisionError
+#       +-- AssertionError
+#       +-- AttributeError
+#       +-- BufferError
+#       +-- EOFError
+#       +-- ImportError
+#       |    +-- ModuleNotFoundError
+#       +-- LookupError
+#       |    +-- IndexError
+#       |    +-- KeyError
+#       +-- MemoryError
+#       +-- NameError
+#       |    +-- UnboundLocalError
+#       +-- OSError
+#       |    +-- BlockingIOError
+#       |    +-- ChildProcessError
+#       |    +-- ConnectionError
+#       |    |    +-- BrokenPipeError
+#       |    |    +-- ConnectionAbortedError
+#       |    |    +-- ConnectionRefusedError
+#       |    |    +-- ConnectionResetError
+#       |    +-- FileExistsError
+#       |    +-- FileNotFoundError
+#       |    +-- InterruptedError
+#       |    +-- IsADirectoryError
+#       |    +-- NotADirectoryError
+#       |    +-- PermissionError
+#       |    +-- ProcessLookupError
+#       |    +-- TimeoutError
+#       +-- ReferenceError
+#       +-- RuntimeError
+#       |    +-- NotImplementedError
+#       |    +-- RecursionError
+#       +-- SyntaxError
+#       |    +-- IndentationError
+#       |         +-- TabError
+#       +-- SystemError
+#       +-- TypeError
+#       +-- ValueError
+#       |    +-- UnicodeError
+#       |         +-- UnicodeDecodeError
+#       |         +-- UnicodeEncodeError
+#       |         +-- UnicodeTranslateError
+#       +-- Warning
+#            +-- DeprecationWarning
+#            +-- PendingDeprecationWarning
+#            +-- RuntimeWarning
+#            +-- SyntaxWarning
+#            +-- UserWarning
+#            +-- FutureWarning
+#            +-- ImportWarning
+#            +-- UnicodeWarning
+#            +-- BytesWarning
+#            +-- EncodingWarning
+#            +-- ResourceWarning
+
+#NOTE!!
+
+# if using parent name it will cover ALL exception in it
+# so 
+# RuntimeError will include both exceptions in it
--- main.py
+++ main.py
@@ -1,27 +1,5 @@
-def put_percent_to_difference(difference):
-    if int(difference) > 0:
-        return f'+{difference}%'
-    return f'{difference}%'
-
-
-def difference_interpretation(difference):
-    if int(difference) >= 50:
-        return 'great'
-    elif int(difference) >= 25:
-        return 'decent'
-    elif int(difference) >= 0:
-        return 'need follow up'
-    return ('critical')
-
-
-months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-revenues = [1500, 1000, 2000, 1800, 3000, 3900, 6500, 8000, 6500, 5900, 5400, 6000]
-costs = [500, 100, 250, 1000, 340, 580, 670, 790, 600, 1200, 1500, 200]
-profits = list(map(lambda x, y: x - y, revenues, costs))
-raw_differences = ['%.d' % (profits[i] / profits[i - 1] * 100 - 100) for i in range(1, len(profits))]
-differences = list(map(put_percent_to_difference, raw_differences))
-interpretations = list(map(difference_interpretation, raw_differences))
-differences = ['n/a'] + differences
-interpretations = ['n/a'] + interpretations
-for month, profit, difference, interpretation in zip(months, profits, differences, interpretations):
-    print(f'{month} {profit} {difference} {interpretation}')
+v1 = tuple(map(int, input().split()))
+v2 = tuple(map(int, input().split()))
+print(f'v1 = {v1} & v2 = {v2}')
+for v1coord, v2coord in zip(v1, v2):
+  print(v1coord + v2coord)
