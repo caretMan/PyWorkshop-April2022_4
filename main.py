e49bf445bca4df664cc9cdf8c875b15b9fe5e74d
Dmitry G <103391790+dmitryg65@users.noreply.github.com>
2022-04-13 01:14:45 +0000

    05 - 5

--- main.py
+++ main.py
@@ -1,5 +1,1 @@
-v1 = tuple(map(int, input().split()))
-v2 = tuple(map(int, input().split()))
-print(f'v1 = {v1} & v2 = {v2}')
-for v1coord, v2coord in zip(v1, v2):
-  print(v1coord + v2coord)
+# StopIteration
