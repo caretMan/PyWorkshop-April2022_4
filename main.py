db93a9814b1b0a8e0677fe823945e7dcc81816ea
Dmitry G <103391790+dmitryg65@users.noreply.github.com>
2022-04-14 02:51:46 +0000

    06 - 4

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+from time import perf_counter
+
 N = int(input())
+start = perf_counter()
 D = []
-for i in range(2, N):
-  if N % i == 0:
+i = 2
+while i ** 2 <= N:
+  if (i % 6 == 1 or i % 6 == 5) and N % i == 0:
     k = 2
-    while k < i:
+    while k ** 2 <= i:
       if i % k == 0:
         k = -1
         break
       k += 1
     if k != -1:
       D.append(i)
+  i += 1
 print(' '.join(list(map(str, D))))
+print(f'\nTime: {perf_counter() - start} seconds')
+
+#Time for 1_000_000_000: 0.09607347099881736 seconds
