a7afadc1dc61ea2d4d78610decc1315d8817e669
Dmitry G <103391790+dmitryg65@users.noreply.github.com>
2022-04-15 00:37:49 +0000

    08 - 7 - 1

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-import sys
-
 try:
-  a = int(input())
-  b = int(input())
-  c = int(input())
-  if c == 1:
-    print(d)
-  else:
-    print('%.2f' % (a / b))
-except (NameError, ValueError, ZeroDivisionError) as err:
-  err_type, err_object, err_traceback = sys.exc_info()
-  err_line = err_traceback.tb_lineno
-  print(f'{err.__class__.__name__} exception on line {err_line}!')
+  r = 5 // 1
+except ZeroDivisionError:
+  print('Error!')
+else:
+  print(f'Result = {r}')
+finally:
+  print('Thank you for using this program')
