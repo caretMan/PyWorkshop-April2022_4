98765f5b1ba6f327e1b53a73c3b193060e8d37f6
Dmitry G <103391790+dmitryg65@users.noreply.github.com>
2022-04-20 00:02:55 +0000

    12 - 5

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-# -> Functions to generate pseudo-random numbers.
+from random import randint
+
+dices = []
+while dices != [5, 5] and dices != [6, 6]:
+  dices = [randint(1, 6) for _ in range(2)]
+print(' '.join(list(map(str, dices))))
