7bd3ca5c51b5892faf21e796ce0ef5ce1c33718b
Dmitry G <103391790+dmitryg65@users.noreply.github.com>
2022-04-20 10:01:23 +0000

    14 - 3

--- main.py
+++ main.py
@@ -1,31 +1,15 @@
-def binary(Step, Current):
-  Sum, Sum1 = 0, 0
-  for i in range(len(L)):
-    Sum += L[i] // Current
-  for i in range(len(L)):
-    Sum1 += L[i] // (Current + 1)
-  if Step <= 1:
-    Step = 2
-  if Sum >= K:
-    if Sum1 < K:
-      return Current
-    Step = Step // 2
-    return binary(Step, Current + Step)
-  if Sum < K:
-    Step = Step // 2
-    return binary(Step, Current - Step)
+def binary(A, step):
+  if step == 0:
+    step = 2
+  if N >= A ** 3 and N < (A + 1) ** 3:
+    return A
+  if N > A ** 3:
+    step = step // 2
+    return binary(A + step, step)
+  if N < A ** 3:
+    step = step // 2
+    return binary(A - step, step)
 
 
-N, K = map(int, input().split())
-L = []
-Ltotalsum = 0
-for i in range(N):
-  L.append(int(input()))
-  Ltotalsum += L[-1]
-Lmax = max(L)
-Step = Lmax // 2
-Current = max(L) // 2
-if Ltotalsum >= K:
-  print(binary(Step, Current))
-else:
-  print(0)
+N = int(input())
+print(binary(N // 2, N))
