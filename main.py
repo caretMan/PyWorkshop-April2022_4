8a98160b92a754e667316144a35fe0bdeaba7ed9
Dmitry G <103391790+dmitryg65@users.noreply.github.com>
2022-04-20 02:06:45 +0000

    13 - 8

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-random_dict = {'key1': 'value1', 'key2': 'value2', 'key3': 'value3'}
-word = input()
-try:
-  print(random_dict[word])
-except KeyError:
-  print('Not in the dictionary')
+from collections import Counter
+
+
+text = ("all I want is a proper cup of coffee made in a proper copper coffee pot. "
+        "I may be off my dot but I want a cup of coffee from a proper coffee pot.")
+
+n = int(input())
+List_C = Counter(text.split())
+for i in range(n):
+  print(List_C.most_common(n)[i][0], List_C.most_common(n)[i][1])
