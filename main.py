8191e7f886b33b01d41ed3b713bce26c98b48a10
Dmitry G <103391790+dmitryg65@users.noreply.github.com>
2022-04-20 23:51:40 +0000

    15 - 2

--- main.py
+++ main.py
@@ -1,5 +1,2 @@
-with open('summers and other things.txt', 'r', encoding='utf-8') as file:
-  strs = file.readlines()
-  print(strs)
-  summers = strs.count('summer\n') + strs.count('summer')
-  print(summers)
+with open('passwords.txt', 'r', encoding='utf-8') as file:
+  print(len(file.readlines()))
