282f354c4b95ba61852632f7cfcb8a5f45033aee
Dmitry G <103391790+dmitryg65@users.noreply.github.com>
2022-04-20 12:44:10 +0000

    14 - 4

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 def binary(A, step):
+  A_without_unlucky = A - A // 13 - A // 17 + A // (13 * 17)
+  A1_without_unlucky = (A + 1) - (A + 1) // 13 - (A + 1) // 17 + (A + 1) // (13 * 17)
   if step == 0:
     step = 2
-  if N >= A ** 3 and N < (A + 1) ** 3:
+  if N >= A_without_unlucky and N < A1_without_unlucky:
     return A
-  if N > A ** 3:
+  if N > A_without_unlucky:
     step = step // 2
     return binary(A + step, step)
-  if N < A ** 3:
+  if N <= A1_without_unlucky:
     step = step // 2
     return binary(A - step, step)
 
-
 N = int(input())
-print(binary(N // 2, N))
+print(binary(3 * N // 2, N))
