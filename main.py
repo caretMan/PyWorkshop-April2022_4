b9a3d0169f5f4d4463208cc34e017219a07ab7fd
Dmitry G <103391790+dmitryg65@users.noreply.github.com>
2022-04-20 01:22:36 +0000

    13 - 5

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from collections import defaultdict
+from collections import Counter
 
-
-transactions = [(38177, 34.38), (876, 999.99), (654276, 653678), (54366, 0.99), (546, 987.65), (876, 3456), (654276, 0.55), (38177, 876.75), (876, 98.7)]
-transaction_dict = defaultdict(list)
-for i, e in enumerate(transactions):
-  transaction_dict[transactions[i][0]] = transaction_dict[transactions[i][0]] + [transactions[i][1]]
-print(transaction_dict)
+List = input().split()
+List_C = Counter(List)
+print(List_C.most_common(1)[0][0])
