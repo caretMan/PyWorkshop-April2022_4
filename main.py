d9e49d3a9c2e1121805cf89593834ab1af81ea7b
Dmitry G <103391790+dmitryg65@users.noreply.github.com>
2022-04-21 01:11:08 +0000

    15 - 8

--- main.py
+++ main.py
@@ -1,4 +1,2 @@
-with open('planets.txt', 'w', encoding='utf-8') as file:
-  planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
-  for planet in planets:
-    file.write(planet + '\n')
+with open('countries.txt', 'a', encoding='utf-8') as file:
+  file.write('Turkey\n')
