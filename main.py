9c0603578449e1b719f753d294a010dd9872e8d5
Dmitry G <103391790+dmitryg65@users.noreply.github.com>
2022-04-21 01:06:06 +0000

    15 - 7

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-# cyanmagentayellowkey color
+with open('planets.txt', 'w', encoding='utf-8') as file:
+  planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+  for planet in planets:
+    file.write(planet + '\n')
