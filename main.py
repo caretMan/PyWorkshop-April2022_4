cf4d952c30c0ae5b2a47fc5abea0f700ca618127
Dmitry G <103391790+dmitryg65@users.noreply.github.com>
2022-04-21 01:33:46 +0000

    15 - 9

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-with open('countries.txt', 'a', encoding='utf-8') as file:
-  file.write('Turkey\n')
+with open('passwords.txt', 'r', encoding='utf-8') as file:
+  letters = {}
+  passwords = []
+  for line in file:
+    password = line.strip()
+    passwords.append(password)
+    for i in range(len(password)):
+      letters.setdefault(password[i], 0)
+      letters[password[i]] += 1
+  score = []
+  for password in passwords:
+    score.append(0)
+    for i in range(len(password)):
+      score[-1] += letters[password[i]]
+
+  print(passwords[score.index(min(score))])
